be5c761132c5d615bee962e54fbb78f165905ca2
Hakan Yücelşen <hakanyucelsen@gmail.com>
2016-12-18 01:04:27 +0200

    change data attr, add image width and height

--- js/center-image.js
+++ js/center-image.js
@@ -6,26 +6,32 @@
         }, options);
 
         var el = $(this);
-        el.each( function () {
-            var imageSrc    = $(this).attr('src');
-            var imgAlt    = $(this).attr('alt');
-            var imgClass = $(this).attr('class');
+        el.each(function () {
+            var imageSrc = $(this).attr('src');
+            var imageAlt = $(this).attr('alt');
+            var imageClass = $(this).attr('class');
+            var imageTop = $(this).height() / 2;
+            var imageLeft = $(this).width() / 2;
 
-            if ( $(this).attr('data-responsive') == 'true' ||  $(this).attr('data-responsive') === undefined ) {
-                if ( $(window).width() < options.width ) {
-                    var canvasHeight = 'auto';
-                    var imgstyle = "position: relative; max-width: 100%;";
-                } else {
-                    var canvasHeight = $(this).attr('data-height')+'px';
-                    var imgWidth = $(this).attr('data-width')+'px';
-                    var imgstyle = "transform: translate(-50%, -50%);position: absolute;left: 50%;top: 50%; width: "+ imgWidth +"";
-                }
+            var imageHeight = $(this).attr('data-image-height');
+            var imageWidth = $(this).attr('data-image-width');
+
+            var canvasHeight = $(this).attr('data-height');
+            var canvasWidth = $(this).attr('data-width') ? $(this).attr('data-width')+'px' : '100%';
+
+            if ($(window).width() < options.width) {
             } else {
-                var canvasHeight = 'auto';
-                var imgstyle = "position: relative; max-width: 100%;";
+                var height = imageHeight === undefined ? '' : 'height: '+ imageHeight +';';
+                var width = imageWidth === undefined ? '' : 'width: '+ imageWidth +';';
+
+                var canvasStyle = 'width: '+ canvasWidth +'; height: '+ canvasHeight +'px; position: relative; overflow: hidden;';
+                var imgstyle = 'position: absolute; top: 50%; left: 50%; transform: translate(-50%,-50%); '+ height +' '+ width +' ';
             }
 
-            $(this).before('<div class="center-image-canvas" style="height: ' + canvasHeight + '; overflow: hidden; position: relative;"><img style="'+ imgstyle +'" src="' + imageSrc + '" alt="' + imgAlt + '" class="center-image '+ imgClass +'"  /></div>');
+            $(this).before('' +
+                '<div class="center-image-canvas" style="'+ canvasStyle +'">' +
+                '<img style="' + imgstyle + '" src="' + imageSrc + '" alt="' + imageAlt + '" class="center-image ' + imageClass + '"  />' +
+                '</div>');
             $(this).remove();
         });
     };
