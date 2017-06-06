5ea7bc72ce4423235197467651f13fafb130ecd7
Hakan Yücelşen <hakan.yucelsen@arma.net.tr>
2017-06-06 15:51:35 +0300

    responsive active and deactive window resize

--- js/center-image.js
+++ js/center-image.js
@@ -2,7 +2,7 @@
     $.fn.centerImage = function (options) {
 
         var options = $.extend({
-            'width': 992
+            'responsive': [1024,768]
         }, options);
 
         var el = $(this);
@@ -13,26 +13,73 @@
             var imageTop = $(this).height() / 2;
             var imageLeft = $(this).width() / 2;
 
-            var imageHeight = $(this).attr('data-image-height');
-            var imageWidth = $(this).attr('data-image-width');
+            var imageHeight = $(this).attr('data-image-height') ? $(this).attr('data-image-height') : 'auto';
+            var imageWidth = $(this).attr('data-image-width') ? $(this).attr('data-image-width') : 'auto';
 
-            var canvasHeight = $(this).attr('data-height');
-            var canvasWidth = $(this).attr('data-width') ? $(this).attr('data-width')+'px' : '100%';
+            var tabletActive = $(this).attr('data-sm');
+            var mobileActive = $(this).attr('data-xs');
 
-            if ($(window).width() < options.width) {
-            } else {
+            var canvasHeight = $(this).attr('data-height') ? $(this).attr('data-height') : '100%';
+            var canvasWidth = $(this).attr('data-width') ? $(this).attr('data-width') : '100%';
+
+            if ($(window).width() > 0 && $(window).width() < options.responsive[1]) {
+                // Phone
+                if (mobileActive == 'true') {
+                    var height = imageHeight === undefined ? '' : 'height: '+ imageHeight +';';
+                    var width = imageWidth === undefined ? '' : 'width: '+ imageWidth +';';
+
+                    var canvasStyle = 'width: '+ canvasWidth +'; height: '+ canvasHeight +'; position: relative; overflow: hidden;';
+                    var imgstyle = 'position: absolute; top: 50%; left: 50%; transform: translate(-50%,-50%); '+ height +' '+ width +' ';
+                }
+                else {
+                    var canvasStyle = 'position: relative; overflow: hidden;';
+                    var imgstyle = 'max-width: 100%;';
+                }
+            }
+            else if ($(window).width() > options.responsive[1] && $(window).width() < options.responsive[0]) {
+                // Tablet
+                if (tabletActive == 'true') {
+                    var height = imageHeight === undefined ? '' : 'height: '+ imageHeight +';';
+                    var width = imageWidth === undefined ? '' : 'width: '+ imageWidth +';';
+
+                    var canvasStyle = 'width: '+ canvasWidth +'; height: '+ canvasHeight +'; position: relative; overflow: hidden;';
+                    var imgstyle = 'position: absolute; top: 50%; left: 50%; transform: translate(-50%,-50%); '+ height +' '+ width +' ';
+                }
+                else {
+                    var canvasStyle = 'position: relative; overflow: hidden;';
+                    var imgstyle = 'max-width: 100%;';
+                }
+            }
+            else {
                 var height = imageHeight === undefined ? '' : 'height: '+ imageHeight +';';
                 var width = imageWidth === undefined ? '' : 'width: '+ imageWidth +';';
 
-                var canvasStyle = 'width: '+ canvasWidth +'; height: '+ canvasHeight +'px; position: relative; overflow: hidden;';
+                var canvasStyle = 'width: '+ canvasWidth +'; height: '+ canvasHeight +'; position: relative; overflow: hidden;';
                 var imgstyle = 'position: absolute; top: 50%; left: 50%; transform: translate(-50%,-50%); '+ height +' '+ width +' ';
             }
 
-            $(this).before('' +
-                '<div class="center-image-canvas" style="'+ canvasStyle +'">' +
-                '<img style="' + imgstyle + '" src="' + imageSrc + '" alt="' + imageAlt + '" class="center-image ' + imageClass + '"  />' +
-                '</div>');
-            $(this).remove();
+            if ($(this).parent().hasClass('center-image-canvas')) {
+                $(this).attr({
+                    style: imgstyle
+                });
+                $(this).parent().attr({
+                    style: canvasStyle
+                })
+            } else {
+                $(this).before('' +
+                    '<div class="center-image-canvas" style="'+ canvasStyle +'">' +
+                    '<img data-height="'+ canvasHeight +'" data-width="'+ canvasWidth +'" data-image-height="'+ imageHeight +'" data-image-width="'+ imageWidth +'" data-sm="'+ tabletActive +'" data-xs="'+ mobileActive +'" style="' + imgstyle + '" src="' + imageSrc + '" alt="' + imageAlt + '" class="' + imageClass + '"  />' +
+                    '</div>');
+                $(this).remove();
+            }
         });
     };
+
+    $(document).ready(function () {
+        $('.center-image').centerImage();
+    });
+
+    $(window).resize(function () {
+        $('.center-image').centerImage();
+    });
 }(jQuery));
